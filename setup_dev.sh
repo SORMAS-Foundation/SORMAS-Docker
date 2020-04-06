b937c3ad22d13c07804d59eb9b6117ef2b4feb68
Adrian Sorge <aso@localhost.localdomain>
2020-04-06 14:02:30 +0100

    fxed typo in path to yaml files

--- setup_dev.sh
+++ setup_dev.sh
@@ -6,5 +6,5 @@ cp /home/deploy/docker-compose.yml /home/deploy/SORMAS-Docker/
 
 sed -i "s/SORMAS_VERSION=.*/SORMAS_VERSION=$1/" /home/deploy/SORMAS-Docker/.env
 
-docker-compose -f /home/deploy/SORMAS-Docker/docker-compose-build.yaml build
-docker-compose -f /home/deploy/SORMAS-Docker/docker-compose.yaml up -d
+docker-compose -f /home/deploy/SORMAS-Docker/docker-compose-build.yml build
+docker-compose -f /home/deploy/SORMAS-Docker/docker-compose.yml up -d
