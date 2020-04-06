28a8c95b425e6a02cca24a0c86ac0440def74101
Adrian Sorge <aso@localhost.localdomain>
2020-04-06 13:59:29 +0100

    added build

--- setup_dev.sh
+++ setup_dev.sh
@@ -5,3 +5,6 @@ cp /home/deploy/.env /home/deploy/SORMAS-Docker/
 cp /home/deploy/docker-compose.yml /home/deploy/SORMAS-Docker/
 
 sed -i "s/SORMAS_VERSION=.*/SORMAS_VERSION=$1/" /home/deploy/SORMAS-Docker/.env
+
+docker-compose -f /home/deploy/SORMAS-Docker/docker-compose-build.yaml build
+docker-compose -f /home/deploy/SORMAS-Docker/docker-compose.yaml up -d
