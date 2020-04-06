d28ea661f14474f105b1da45baaf7ce0e4d79fd6
Adrian Sorge <aso@localhost.localdomain>
2020-04-06 13:55:08 +0100

    fixed typo

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-cp /home/deploy/.env /home/deploy/SORMAS-docker/
-cp /home/deploy/docker-compose.yml /home/deploy/SORMAS-docker/
+cp /home/deploy/.env /home/deploy/SORMAS-Docker/
+cp /home/deploy/docker-compose.yml /home/deploy/SORMAS-Docker/
 
-sed -i "s/SORMAS_VERSION=.*/SORMAS_VERSION=$1/" /home/deploy/SORMAS-docker/.env
+sed -i "s/SORMAS_VERSION=.*/SORMAS_VERSION=$1/" /home/deploy/SORMAS-Docker/.env
