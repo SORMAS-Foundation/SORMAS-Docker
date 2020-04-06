279679b712ca876b365a000a7abaff4019b4ae98
Adrian Sorge <aso@localhost.localdomain>
2020-04-06 14:16:48 +0100

    added force to docker image prune

--- setup_dev.sh
+++ setup_dev.sh
@@ -6,6 +6,6 @@ cp /home/deploy/docker-compose.yml /home/deploy/SORMAS-Docker/
 
 sed -i "s/SORMAS_VERSION=.*/SORMAS_VERSION=$1/" /home/deploy/SORMAS-Docker/.env
 sudo docker-compose -f /home/deploy/SORMAS-Docker/docker-compose.yml down
-sudo docker image prune
+sudo docker image prune -f -a
 sudo docker-compose -f /home/deploy/SORMAS-Docker/docker-compose-build.yml build
 sudo docker-compose -f /home/deploy/SORMAS-Docker/docker-compose.yml up -d
