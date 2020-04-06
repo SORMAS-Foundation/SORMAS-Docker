1ab8d301983d5398ba10fe34eb450f0401fdc2eb
Adrian Sorge <aso@localhost.localdomain>
2020-04-06 14:14:29 +0100

    added docker compose down and docker image prune

--- setup_dev.sh
+++ setup_dev.sh
@@ -5,6 +5,7 @@ cp /home/deploy/.env /home/deploy/SORMAS-Docker/
 cp /home/deploy/docker-compose.yml /home/deploy/SORMAS-Docker/
 
 sed -i "s/SORMAS_VERSION=.*/SORMAS_VERSION=$1/" /home/deploy/SORMAS-Docker/.env
-
+sudo docker-compose -f /home/deploy/SORMAS-Docker/docker-compose.yml down
+sudo docker image prune
 sudo docker-compose -f /home/deploy/SORMAS-Docker/docker-compose-build.yml build
 sudo docker-compose -f /home/deploy/SORMAS-Docker/docker-compose.yml up -d
