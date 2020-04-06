419417e1a76aa18a7f2396538eebdb080bf0cd57
Adrian Sorge <aso@localhost.localdomain>
2020-04-06 13:41:05 +0100

    fix for new directory

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-cp /root/.env /root/SORMAS-docker/
-cp /root/docker-compose.yml /root/SORMAS-docker/
+cp /home/deploy/.env /home/deploy/SORMAS-docker/
+cp /home/deploy/docker-compose.yml /home/deploy/SORMAS-docker/
 
-sed -i "s/SORMAS_VERSION=.*/SORMAS_VERSION=$1/" /root/SORMAS-docker/.env
+sed -i "s/SORMAS_VERSION=.*/SORMAS_VERSION=$1/" /home/deploy/SORMAS-docker/.env
