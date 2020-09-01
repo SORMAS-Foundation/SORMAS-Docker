aa97f1b80f447300d057896872d684605a51f399
nliakm <amini@netzlink.com>
2020-09-01 16:10:42 +0200

    Allow usage of payara metrics endpoint

--- apache2/vhost.conf.sh
+++ apache2/vhost.conf.sh
@@ -19,7 +19,7 @@ Listen 443
 <VirtualHost *:443>
         ServerName ${SORMAS_SERVER_URL}
 
-	RedirectMatch "^(/(?!downloads).*)" https://${SORMAS_SERVER_URL}/sormas-ui\$1
+	RedirectMatch "^(/(?!downloads|metrics).*)" https://${SORMAS_SERVER_URL}/sormas-ui\$1
 	
         ErrorLog /var/log/apache2/error.log
         LogLevel warn
@@ -43,6 +43,13 @@ Listen 443
         ProxyPassReverse /sormas-ui http://sormas:6080/sormas-ui
         ProxyPass /sormas-rest http://sormas:6080/sormas-rest connectiontimeout=5 timeout=600
         ProxyPassReverse /sormas-rest http://sormas:6080/sormas-rest
+        <Location /metrics>
+            ProxyPass  http://sormas:6080/metrics connectiontimeout=5 timeout=600
+            ProxyPassReverse http://sormas:6080/metrics
+            Order deny,allow
+            Deny from all
+            Allow from ${PROMETHEUS_SERVERS}
+        </Location>
         RequestHeader set X-Forwarded-Proto https
 
         Options -Indexes
