6ac547211dfdf79756149bfc900f3312063ca8cb
nliakm <amini@netzlink.com>
2020-10-05 17:21:01 +0200

    Added sormas2sormas environments. Prepare for release 2.11.0

--- sormas/start-server.sh
+++ sormas/start-server.sh
@@ -159,7 +159,10 @@ echo -e "\ncustombranding.loginbackground.path=${CUSTOMBRANDING_LOGINBACKGROUND_
 fi
 if [ ! -z "$SORMAS2SORMAS_ENABLED" ];then
 echo -e "\nsormas2sormas.keyAlias = ${SORMAS2SORMAS_KEYALIAS}" >>${DOMAIN_DIR}/sormas.properties
-echo -e "\nsormas2sormas.keyPassword = ${SORMAS2SORMAS_KEYPASSWORD}" >>${DOMAIN_DIR}/sormas.properties
+echo -e "\nsormas2sormas.keystoreName = ${SORMAS2SORMAS_KEYSTORENAME}" >>${DOMAIN_DIR}/sormas.properties
+echo -e "\nsormas2sormas.keystorePass = ${SORMAS2SORMAS_KEYPASSWORD}" >>${DOMAIN_DIR}/sormas.properties
+echo -e "\nsormas2sormas.truststoreName = ${SORMAS2SORMAS_TRUSTSTORENAME}" >>${DOMAIN_DIR}/sormas.properties
+echo -e "\nsormas2sormas.truststorePass = ${SORMAS2SORMAS_TRUSTSTOREPASSWORD}" >>${DOMAIN_DIR}/sormas.properties
 fi
 
 
