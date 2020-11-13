a197abc00e28690440ecb1e08e8ad7717d1c93d3
Tobias Hundt <hundt@netzlink.com>
2020-11-13 10:28:19 +0100

    fixed geo location

--- sormas/start-server.sh
+++ sormas/start-server.sh
@@ -151,12 +151,13 @@ sed -i "s/map.zoom=.*/map.zoom=${MAP_ZOOM}/" ${DOMAIN_DIR}/sormas.properties
 sed -i "s;app.url=.*;app.url=https://${SORMAS_SERVER_URL}/downloads/release/sormas-${SORMAS_VERSION}-release.apk;" ${DOMAIN_DIR}/sormas.properties
 
 #------------------GEOCODING
-sed -i "s/^geocodingServiceUrlTemplate.*/d " ${DOMAIN_DIR}/sormas.properties
-sed -i "s/^geocodingLongitudeJsonPath.*/d " ${DOMAIN_DIR}/sormas.properties
-sed -i "s/^geocodingLatitudeJsonPath.*/d " ${DOMAIN_DIR}/sormas.properties
+sed -i "/^geocodingServiceUrlTemplate/d " ${DOMAIN_DIR}/sormas.properties
+sed -i "/^geocodingLongitudeJsonPath/d " ${DOMAIN_DIR}/sormas.properties
+sed -i "/^geocodingLatitudeJsonPath/d " ${DOMAIN_DIR}/sormas.properties
 echo -e "geocodingServiceUrlTemplate=${GEO_TEMPLATE}" >>${DOMAIN_DIR}/sormas.properties
 echo -e "geocodingLongitudeJsonPath=${GEO_LONG_TEMPLATE}" >>${DOMAIN_DIR}/sormas.properties
 echo -e "geocodingLatitudeJsonPath=${GEO_LAT_TEMPLATE}" >>${DOMAIN_DIR}/sormas.properties
+sed -i "s/\${GEO_UUID}/${GEO_UUID}/" ${DOMAIN_DIR}/sormas.properties
 
 sed -i "s/\#rscript.executable=.*/rscript.executable=Rscript/" ${DOMAIN_DIR}/sormas.properties
 sed -i "s/\#\s\devmode=.*/devmode=${DEVMODE}/" ${DOMAIN_DIR}/sormas.properties
