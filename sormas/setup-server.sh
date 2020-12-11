1efd86d0af13c40098ebcb5721d2bb53646b6f0a
akm <amini@netzlink.com>
2020-12-11 11:35:30 +0100

    Set documents logging level to ERROR

--- sormas/setup-server.sh
+++ sormas/setup-server.sh
@@ -129,6 +129,8 @@ cp ${DEPLOY_PATH}/deploy/s2s-import-to-truststore.sh /opt/sormas/sormas2sormas
 #Der String "300" tritt ausschließlich an Stellen auf an denen die (txn-)Timeouts definiert werden. Diese werden auf "0" gesetzt um die Timeouts zu deaktivieren
 echo "Configure payara timeouts ..."
 sed -i 's/"300"/"0"/g' ${DOMAIN_DIR}/config/domain.xml
+echo "Set logging fo documents to ERROR level"
+sed -i '/^<\/configuration>/i <\/root>\n<logger name="fr.opensagres.xdocreport" level="ERROR" \/>' ${DOMAIN_DIR}/config/logback.xml
 
 
 ${PAYARA_HOME}/bin/asadmin stop-domain --domaindir ${DOMAINS_HOME}
