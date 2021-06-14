e29fe9a0cf3c249ae40a8de189f58222ab523fbd
Tomasz Nakonieczny <tomasz.nakonieczny@softgent.com>
2021-06-14 17:15:40 +0200

    More phrases to patch from logback.xml once at in single quotations

--- sormas/setup-server.sh
+++ sormas/setup-server.sh
@@ -134,10 +134,14 @@ sed -i 's/"300"/"0"/g' ${DOMAIN_DIR}/config/domain.xml
 # echo "Set logging fo documents to WARNING level"
 # sed -i '/<root level="debug">/i\ \ \ \ <logger name="fr.opensagres.xdocreport" level="WARN" />' ${DOMAIN_DIR}/config/logback.xml
 
-# Patching logback.xml - there is an assumption that searched phrase exists - if not - should exit
+# Patching logback.xml - there is an assumption that searched phrases exist - if not - should exit
 echo "Patching logback.xml configuration"
 set -x
-grep "^[[:blank:]]*<smtpHost>localhost</smtpHost>[[:blank:]]*$" ${DOMAIN_DIR}/config/logback.xml
+grep '^[[:blank:]]*<smtpHost>localhost</smtpHost>[[:blank:]]*$' ${DOMAIN_DIR}/config/logback.xml
+grep '<!-- <appender-ref ref="EMAIL_ERROR" /> -->' ${DOMAIN_DIR}/config/logback.xml
+grep '<subject>SORMAS: %logger{20} - %m</subject>' ${DOMAIN_DIR}/config/logback.xml
+sed -i 's|<subject>SORMAS: %logger{20} - %m</subject>|<!-- <subject>SORMAS: %logger{20} - %m</subject> -->|g' ${DOMAIN_DIR}/config/logback.xml
+sed -i 's|<smtpHost>localhost</smtpHost>|\t<smtpHost>MAIL_HOST</smtpHost>\n\t\t<smtpPort>SMTP_PORT</smtpPort>\n\t\t<username>SMTP_USER</username>\n\t\t<password>SMTP_PASSWORD</password>\n\t\t<STARTTLS>SMTP_STARTTLS</STARTTLS>\n\t\t<SSL>SMTP_SSL</SSL>\n\t\t<asynchronousSending>SMTP_ASYNC_SENDING</asynchronousSending>\n\t\t<to>LOG_RECIPIENT_ADDRESS</to>\n\t\t<from>LOG_SENDER_ADDRESS</from>\n\t\t<subject>LOG_SUBJECT</subject>|g' ${DOMAIN_DIR}/config/logback.xml
 set +x
 
 ${PAYARA_HOME}/bin/asadmin stop-domain --domaindir ${DOMAINS_HOME}
