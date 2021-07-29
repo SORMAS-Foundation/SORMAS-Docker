86f81c715aabb0dfbc96b43571945071024feafd
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2021-07-29 12:36:11 +0200

    remove redis and add etcd

--- central/redis/redis-setup.sh
+++
@@ -1,8 +0,0 @@
-redis-server /usr/local/etc/redis/config/redis.conf --daemonize yes --tls-auth-clients no && sleep 1
-
-redis-cli --user default --tls --cacert /srv/tls/ca/public/ca.crt  < /srv/fixtures/s2s-access-data.redis || exit 1
-redis-cli --user default --tls --cacert /srv/tls/ca/public/ca.crt save || exit 1
-redis-cli --user default --tls --cacert /srv/tls/ca/public/ca.crt shutdown || exit 1
-
-
-redis-server /usr/local/etc/redis/config/redis.conf
