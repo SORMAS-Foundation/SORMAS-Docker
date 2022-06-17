8f7e3bf00a54beb378a80e263cb28397ed64527f
Frank Hauptmann <f.hauptmann@in2solutions.de>
2022-06-17 14:31:56 +0200

    HSP-3053 LBDS

--- apache2/vhost.conf.sh
+++ apache2/vhost.conf.sh
@@ -23,7 +23,7 @@ Listen 443
         # RedirectMatch: All locations not listed here will be redirected to sormas-ui
         # APACHE_REDIRECT_EXCLUDE Example Usage: "|test|test2" will add /test and /test2 to that list
         # IMPORTANT: it needs to start with "|"
-	    RedirectMatch "^(/(?!downloads|keycloak|metrics${REDIRECT_MATCH_LBDS_CONTEXT_PATH}).*)" https://${SORMAS_SERVER_URL}/sormas-ui\$1
+	    RedirectMatch "^(/(?!downloads|keycloak|metrics${APACHE_REDIRECT_EXCLUDE}).*)" https://${SORMAS_SERVER_URL}/sormas-ui\$1
 		
         ErrorLog /var/log/apache2/error.log
         LogLevel warn
