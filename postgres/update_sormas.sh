1969edb90a4c39124f6e3b21f25f9ed66a3762c8
Frank Hauptmann <hauptmann@netzlink.com>
2023-02-21 13:56:21 +0100

    HSP-4167 fix sql script to connect to database

--- postgres/update_sormas.sh
+++ postgres/update_sormas.sh
@@ -5,5 +5,6 @@ set -e
 echo "Starting database modification..."
 
 psql -v ON_ERROR_STOP=1 --username "postgres" <<EOSQL
+    \c ${DB_NAME}
     ALTER FUNCTION public.versioning() OWNER TO ${SORMAS_POSTGRES_USER};
 EOSQL
