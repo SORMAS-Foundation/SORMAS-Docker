4225ef5e13d482fe5d75403f231a7d06f6b4aa9a
Frank Hauptmann <hauptmann@netzlink.com>
2023-03-07 13:16:51 +0100

    HSP-4210 don't stop

--- postgres/update_sormas.sh
+++ postgres/update_sormas.sh
@@ -4,7 +4,7 @@ set -e
 # Set up the database
 echo "Starting database modification..."
 
-psql -v ON_ERROR_STOP=1 --username "postgres" <<EOSQL
+psql -v ON_ERROR_STOP=0 --username "postgres" <<EOSQL
     \c ${DB_NAME}
     ALTER FUNCTION public.versioning() OWNER TO ${SORMAS_POSTGRES_USER};
 EOSQL
